bd562b689a0c668d19c0f6071292c0c1b9b0622a
BB <sorawitthassadorn15@gmail.com>
2026-07-01 15:59:32 +0700

    update

---
+++ myApp_673380065_6/src/main/java/com/cp2/app/App.java
@@ -0,0 +1,10 @@
+package com.cp2.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello!  My name is sorawit thassadorn, Stuent ID: 673380065-6 sec:2");
+    }
+}
