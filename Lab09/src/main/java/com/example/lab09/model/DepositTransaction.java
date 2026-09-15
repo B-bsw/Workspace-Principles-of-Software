8421718cd443ce3ba832808499c2e820e76e0c08
BB <sorawitthassadorn15@gmail.com>
2026-09-15 18:59:24 +0700

    Lab09: update model

--- Lab09/src/main/java/com/example/lab09/model/DepositTransaction.java
+++ Lab09/src/main/java/com/example/lab09/model/DepositTransaction.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class DepositTransaction {
@@ -13,5 +15,32 @@ public class DepositTransaction {
     private Long id;
 
     private Double amount;
-    private Long account_id;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
 }
